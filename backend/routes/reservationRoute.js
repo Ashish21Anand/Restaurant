c3ab0f67f3af34c8b478608f5904af5565723647
Ashish <ashish717189@gmail.com>
2025-08-03 01:13:54 +0530

    commit

---
+++ backend/routes/reservationRoute.js
@@ -0,0 +1,8 @@
+import express from "express";
+import send_Reservation from "../controller/reservation.js"; 
+const router= express.Router();
+
+router.post("/send",send_Reservation);
+
+
+export default router;
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">ASHISH</div>
+          <div className="right">
+            <p>Nichitpur,Katras Bazra,Dhanbad</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By ASHISH</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By CodeWithAshish.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
