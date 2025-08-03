c7d9329508b48c13246b018658193143482e98af
Ashish <ashish717189@gmail.com>
2025-08-03 20:29:30 +0530

    cpmmit

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,13 @@
-import dotenv from "dotenv";
-dotenv.config({ path: "./config/config.env" }); // 👈 this must come at the top
-console.log("Frontend URL is:", process.env.FRONTEND_URL);
-
-
 import express from "express";
 import cors from "cors";
-////import dotenv from "dotenv";
+import dotenv from "dotenv";
 import {dbConnection} from "./database/dbConnection.js";
 import {errorMiddleware} from "./error/error.js";
 import reservationRouter from './routes/reservationRoute.js';
 
-//dotenv.config({ path:"./config/config.env"});
+
 const app=express();
-console.log(process.env.FRONTEND_URL);
-console.log("CORS Origin:", process.env.FRONTEND_URL);
-
-
+dotenv.config({ path:"./config/config.env"});
 
 app.use(cors({
     origin:[process.env.FRONTEND_URL],
